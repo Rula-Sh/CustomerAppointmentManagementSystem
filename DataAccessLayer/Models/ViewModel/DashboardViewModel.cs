ce68f94b88925b161b8000e4d57759a59d1efc1d
Rula-Sh <1.rulashaban@gmail.com>
2025-04-23 09:44:08 +0300

    Add Dashboard

---
+++ DataAccessLayer/Models/ViewModel/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models.ViewModel
+{
+    public class DashboardViewModel
+    {
+        public List<User> Users  { get; set; }
+        public List<Service> Services { get; set; }
+        public List<Appointment> Appointments { get; set; }
+    }
+}
---
+++ PresentationLayer/Controllers/DashboardController.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.Models.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        const string usersPath = "~/Views/Admin/Dashboard/Index.cshtml";
+
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var model = new DashboardViewModel
+            {
+                Users = _context.Users.ToList(),
+                Services = _context.Services.ToList(),
+                Appointments = _context.Appointments.ToList(),
+            };
+
+            return View(usersPath, model);
+        }
+    }
+}
