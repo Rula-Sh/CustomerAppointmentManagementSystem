c6cff4949f414ab616f4a723b3f0b39fbde6f29b
Rula-Sh <1.rulashaban@gmail.com>
2025-04-24 12:07:52 +0300

    Add DateOnlyResolver for AutoMapperProfile

---
+++ BusinessLogicLayer/Helpers/DateOnlyResolver.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using DataAccessLayer.Models.ViewModel;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Helpers
+{
+    public class DateOnlyResolver : IValueResolver<DateTimeSlotGroupViewModel, ServiceDate, DateOnly>
+    {
+        public DateOnly Resolve(DateTimeSlotGroupViewModel source, ServiceDate destination, DateOnly destMember, ResolutionContext context)
+        {
+            return DateOnly.ParseExact(source.Date, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+        }
+    }
+}
