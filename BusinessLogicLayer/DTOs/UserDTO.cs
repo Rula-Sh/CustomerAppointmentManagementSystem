8f73a5fdecf27ce9d5aee365dc5308279286cd42
Rula-Sh <1.rulashaban@gmail.com>
2025-04-29 20:41:29 +0300

    Update Namespace, Remove Additional imports and Fix Class Name

--- BusinessLogicLayer/DTOs/UserDTO.cs
+++ BusinessLogicLayer/DTOs/UserDTO.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataAccessLayer.Models.ViewModel
+﻿namespace BusinessLogicLayer.DTOs
 {
-    public class UseDTO
+    public class UserDTO
     {
         public int Id { get; set; }
         public string Email { get; set; }
