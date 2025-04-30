7ef55f9d0c83352cab302f2d8d2d2f0e24985168
Rula-Sh <1.rulashaban@gmail.com>
2025-04-30 20:10:04 +0300

    Add PLAutoMapperProfile

---
+++ PresentationLayer/Mapper/PLAutoMapperProfile.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using BusinessLogicLayer.DTOs;
+using DataAccessLayer.Models;
+using PresentationLayer.ViewModels;
+
+namespace PresentationLayer.Mapper
+{
+    public class PLAutoMapperProfile : Profile
+    {
+        public PLAutoMapperProfile()
+        {
+            // -------------------- GET --------------------
+
+            CreateMap<UserDTO, UserViewModel>();
+
+            CreateMap<ServiceDTO, ServiceViewModel>();
+            CreateMap<DateTimeSlotGroupDTO, DateTimeSlotGroupViewModel>();
+
+            CreateMap<AppointmentDTO, AppointmentViewModel>();
+
+            CreateMap<BookAppointmentDTO, BookAppointmentViewModel>();
+
+            CreateMap<ActiveAppointmentDTO, ActiveAppointmentViewModel>();
+
+            // -------------------- POST --------------------
+            // create a service
+            CreateMap<ServiceViewModel,ServiceDTO>();
+            CreateMap<DateTimeSlotGroupViewModel,DateTimeSlotGroupDTO>();
+            //create an appointment
+            CreateMap<BookAppointmentViewModel, BookAppointmentDTO>();
+
+        }
+    }
+}
