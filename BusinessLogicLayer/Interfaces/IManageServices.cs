79e6f94ef580efdc03a2e96c74efaf4696796225
Rula-Sh <1.rulashaban@gmail.com>
2025-04-30 20:19:59 +0300

    Replace Service Model with ServiceDTO and Remove getSelectedServiceDetails Function

--- BusinessLogicLayer/Interfaces/IManageServices.cs
+++ BusinessLogicLayer/Interfaces/IManageServices.cs
@@ -6,11 +6,11 @@ namespace BusinessLogicLayer.Interfaces
     public interface IManageServices
     {
         Task<List<ServiceDTO>> GetServices();
-        Task addService(Service service);
-        Task<Service> getService(int? id);
-        ServiceDTO getSelectedServiceDetails(Service service);
-        Task DeleteService(Service service);
-        Task<Service> getServiceById(int? id);
+        Task addService(ServiceDTO serviceDTO);
+        Task<ServiceDTO> getService(int? id);
+        //ServiceDTO getSelectedServiceDetails(ServiceDTO service);
+        Task DeleteService(ServiceDTO serviceDTO);
+        Task<ServiceDTO> getServiceById(int? id);
 
         string GetMostBookedServiceName();
         Task<List<string>> GetServicesNames();
