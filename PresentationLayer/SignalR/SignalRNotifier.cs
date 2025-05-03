c2345101a46c47a125942efacdc01486327884ca
Rula-Sh <1.rulashaban@gmail.com>
2025-05-04 00:53:46 +0300

    Add SignalRNotifier to access NotificationHub

---
+++ PresentationLayer/SignalR/SignalRNotifier.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLogicLayer.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+using PresentationLayer.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services
+{
+    public class SignalRNotifier : ISignalRNotifier
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public SignalRNotifier(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task SendNotificationAsync()
+        {
+            await _hubContext.Clients.All.SendAsync("displayNotification","");
+        }
+    }
+}
