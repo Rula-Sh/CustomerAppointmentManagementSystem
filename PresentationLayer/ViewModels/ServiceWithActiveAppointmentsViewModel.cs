6716502b79dac184330d65293584e29950b8e4bf
Rula-Sh <1.rulashaban@gmail.com>
2025-05-09 11:02:41 +0300

    Create ServiceWithActiveAppointmentsViewModel

---
+++ PresentationLayer/ViewModels/ServiceWithActiveAppointmentsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLogicLayer.DTOs;
+
+namespace PresentationLayer.ViewModels
+{
+    public class ServiceWithActiveAppointmentsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Duration { get; set; }
+        public decimal Price { get; set; }
+        public List<DateTimeSlotGroupViewModel> DateTimeSlotGroups { get; set; }
+
+        public int AppointmentId { get; set; }
+        public List<AppointmentViewModel> ActiveAppointments { get; set; }
+    }
+}
