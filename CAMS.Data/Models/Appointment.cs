82b8836fac845c63f616492882dcabc8b1cc5984
Rula-Sh <1.rulashaban@gmail.com>
2025-05-12 11:02:56 +0300

    Place namespace Brackets

--- CAMS.Data/Models/Appointment.cs
+++ CAMS.Data/Models/Appointment.cs
@@ -1,38 +1,39 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace CAMS.Data.Models;
-
-public class Appointment
+namespace CAMS.Data.Models
 {
-    public int Id { get; set; }
+    public class Appointment
+    {
+        public int Id { get; set; }
 
-    // Foreign keys
-    [ForeignKey("Customer")]
-    public int CustomerId { get; set; }
-    [ForeignKey("Employee")]
-    public int EmployeeId { get; set; }
-    [Required]
-    [MaxLength(50)]
-    public string Name { get; set; }
-    [Required]
-    public string Date { get; set; }
-    [Required]
-    public string Status { get; set; }
-    [Required]
-    [MaxLength(200)]
-    public string Notes { get; set; }
-    [Required]
-    public DateTime CreatedAt { get; set; }
+        // Foreign keys
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        [ForeignKey("Employee")]
+        public int EmployeeId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        public string Date { get; set; }
+        [Required]
+        public string Status { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Notes { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
 
-    // Navigation properties
-    // One-to-Many relationship (User[Employee/Customer] and Appointment) *** Foreign keys included on top
-    public virtual User Customer { get; set; }
-    public virtual User Employee { get; set; }
+        // Navigation properties
+        // One-to-Many relationship (User[Employee/Customer] and Appointment) *** Foreign keys included on top
+        public virtual User Customer { get; set; }
+        public virtual User Employee { get; set; }
 
-    // Many-to-Many relationship (Appointment and Service)
-    //public virtual ICollection<AppointmentService> AppointmentServices { get; set; }
-    [ForeignKey("Service")]
-    public int ServiceId { get; set; }
-    public virtual Service Service { get; set; }
+        // Many-to-Many relationship (Appointment and Service)
+        //public virtual ICollection<AppointmentService> AppointmentServices { get; set; }
+        [ForeignKey("Service")]
+        public int ServiceId { get; set; }
+        public virtual Service Service { get; set; }
+    }
 }
