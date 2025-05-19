4e1969f0f9bf097d45e4a01e498b105058c4c5c9
Rula-Sh <1.rulashaban@gmail.com>
2025-05-19 17:21:02 +0300

    Add  ;

--- CAMS.Application/Interfaces/INotificationManagerService.cs
+++ CAMS.Application/Interfaces/INotificationManagerService.cs
@@ -9,7 +9,7 @@ namespace CAMS.Application.Interfaces
         Task CreateNotification(Notification notification);
         //Task CreateNotificationOnServiceDeleteForCustomer(int serviceId);
         Task CreateNotificationForAdminOnServiceAction(int serviceId, string serviceName, ClaimsPrincipal user, string action);
-        Task CreateNotificationForEmployeeOnAppointmentCreateOrDelete(int appointmentId, string action)
+        Task CreateNotificationForEmployeeOnAppointmentCreateOrDelete(int appointmentId, string action);
         Task CreateNotificationForCustomerOnAppointmentStatusChange(int appointmentId);
         Task ReadNotification(int id);
     }
