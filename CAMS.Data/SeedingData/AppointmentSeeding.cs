e280d53410412ea834c62fe5840cd9719048a896
Rula-Sh <1.rulashaban@gmail.com>
2025-05-24 08:45:52 +0300

    Apply DataSeed When There are no Appointments

--- CAMS.Data/SeedingData/AppointmentSeeding.cs
+++ CAMS.Data/SeedingData/AppointmentSeeding.cs
@@ -18,27 +18,29 @@ namespace CAMS.Data.SeedingData
             using var scope = serviceProvider.CreateScope();
             var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
             var services = await context.Services.ToDictionaryAsync(s => s.Name);
-
-
-            var appointmentsToSeed = new List<Appointment>
+            if (!context.Appointments.Any())
             {
-                new Appointment { CustomerId = 4, ProviderId= 1, ServiceId= services["Haircut"].Id, Name="Haircut", Date= $"{DateOnly.FromDateTime(DateTime.Today)} - 8:00 - 9:00", Status="Completed", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today)} 8:28:44 PM"), Notes= "" },
-                new Appointment { CustomerId = 4, ProviderId= 1, ServiceId= services["Hair coloring / dyeing"].Id, Name="Hair coloring / dyeing", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(5))} - 8:30 - 9:00", Status="Approved", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-1))} 8:28:44 PM"), Notes= "" },
-                new Appointment { CustomerId = 4, ProviderId= 1, ServiceId = services["Shave"].Id, Name="Shave", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(4))} - 11:00 - 12:00", Status="Pending", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today)} 8:28:44 PM") , Notes= ""},
 
-                new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Electrical repairs"].Id, Name= "Electrical repairs", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-1))} - 10:00 - 11:30", Status="Completed", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-1))} 8:28:44 PM"), Notes= "" },
-                new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Furniture assembly"].Id, Name= "Furniture assembly", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-2))} - 09:00 - 10:00", Status="Approved", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-2))} 8:28:44 PM"), Notes= "" },
-                new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Appliance repair"].Id, Name= "Appliance repair", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-4))} - 11:00 - 12:00", Status="Completed", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-4))} 8:28:44 PM"), Notes= "" },
-                new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Electrical repairs"].Id, Name= "Electrical repairs", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-3))} - 13:00 - 14:30", Status="Approved", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-3))} 8:28:44 PM"), Notes= "" },
-                new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Furniture assembly"].Id, Name= "Furniture assembly", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-2))} - 10:00 - 11:00", Status="Completed", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-2))} 8:28:44 PM"), Notes= "" },
-                new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Appliance repair"].Id, Name= "Appliance repair", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-4))} - 12:30 - 13:30", Status="Approved", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-4))} 8:28:44 PM"), Notes= "" },
-                new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Electrical repairs"].Id, Name= "Electrical repairs", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-3))} - 15:00 - 16:30", Status="Pending", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-3))} 8:28:44 PM"), Notes= "" },
-                new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Furniture assembly"].Id, Name= "Furniture assembly", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-5))} - 14:00 - 15:00", Status="Completed", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-12))} 8:28:44 PM"), Notes= "" },
-                new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Appliance repair"].Id, Name= "Appliance repair", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-6))} - 09:00 - 10:00", Status="Approved", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-7))} 8:28:44 PM"), Notes= "" },
-            };
+                var appointmentsToSeed = new List<Appointment>
+                {
+                    new Appointment { CustomerId = 4, ProviderId= 1, ServiceId= services["Haircut"].Id, Name="Haircut", Date= $"{DateOnly.FromDateTime(DateTime.Today)} - 8:00 - 9:00", Status="Completed", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today)} 8:28:44 PM"), Notes= "" },
+                    new Appointment { CustomerId = 4, ProviderId= 1, ServiceId= services["Hair coloring / dyeing"].Id, Name="Hair coloring / dyeing", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(5))} - 8:30 - 9:00", Status="Approved", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-1))} 8:28:44 PM"), Notes= "" },
+                    new Appointment { CustomerId = 4, ProviderId= 1, ServiceId = services["Shave"].Id, Name="Shave", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(4))} - 11:00 - 12:00", Status="Pending", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today)} 8:28:44 PM") , Notes= ""},
 
-            context.Appointments.AddRange(appointmentsToSeed);
-            await context.SaveChangesAsync();
+                    new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Electrical repairs"].Id, Name= "Electrical repairs", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-1))} - 10:00 - 11:30", Status="Completed", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-1))} 8:28:44 PM"), Notes= "" },
+                    new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Furniture assembly"].Id, Name= "Furniture assembly", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-2))} - 09:00 - 10:00", Status="Approved", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-2))} 8:28:44 PM"), Notes= "" },
+                    new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Appliance repair"].Id, Name= "Appliance repair", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-4))} - 11:00 - 12:00", Status="Completed", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-4))} 8:28:44 PM"), Notes= "" },
+                    new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Electrical repairs"].Id, Name= "Electrical repairs", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-3))} - 13:00 - 14:30", Status="Approved", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-3))} 8:28:44 PM"), Notes= "" },
+                    new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Furniture assembly"].Id, Name= "Furniture assembly", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-2))} - 10:00 - 11:00", Status="Completed", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-2))} 8:28:44 PM"), Notes= "" },
+                    new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Appliance repair"].Id, Name= "Appliance repair", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-4))} - 12:30 - 13:30", Status="Approved", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-4))} 8:28:44 PM"), Notes= "" },
+                    new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Electrical repairs"].Id, Name= "Electrical repairs", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-3))} - 15:00 - 16:30", Status="Pending", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-3))} 8:28:44 PM"), Notes= "" },
+                    new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Furniture assembly"].Id, Name= "Furniture assembly", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-5))} - 14:00 - 15:00", Status="Completed", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-12))} 8:28:44 PM"), Notes= "" },
+                    new Appointment { CustomerId = 5, ProviderId= 2, ServiceId= services["Appliance repair"].Id, Name= "Appliance repair", Date= $"{DateOnly.FromDateTime(DateTime.Today.AddDays(-6))} - 09:00 - 10:00", Status="Approved", CreatedAt= Convert.ToDateTime($"{DateOnly.FromDateTime(DateTime.Today.AddDays(-7))} 8:28:44 PM"), Notes= "" },
+                };
+
+                context.Appointments.AddRange(appointmentsToSeed);
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
