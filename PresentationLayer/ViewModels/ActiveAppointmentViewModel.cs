3ecf4908b0dda482e6bb1a31dbc3a5f1d5663f53
Rula-Sh <1.rulashaban@gmail.com>
2025-06-16 23:18:39 +0300

    Delete PresentationLayer directory

--- PresentationLayer/ViewModels/ActiveAppointmentViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PresentationLayer.ViewModels
-{
-    public class ActiveAppointmentViewModel
-    {
-        public string CustomerName { get; set; }
-        public string AppointmentDate { get; set; }
-        public string ServiceName { get; set; }
-    }
-}
--- PresentationLayer/ViewModels/TotalAppointmentsPerServiceViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PresentationLayer.ViewModels
-{
-    public class TotalAppointmentsPerServiceViewModel
-    {
-        public string ServiceName { get; set; }
-        public int Count { get; set; }
-    }
-}
